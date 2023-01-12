9e59a9e5d8058b8ebfe00cccb818d1316296975a
Mohd Azmaan <mohdazmaan3@gmail.com>
2023-01-12 23:26:25 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(10,20);
-        p.product(10,20,30);
-        p.product(10.0,20.0);
+        System.out.println(p.product(10,20));
+        System.out.println(p.product(10,20,30));
+        System.out.println(p.product(10.0,20.0));
     }
 }
-class Product{
+class Product {
     public int product(int x, int y) {
         return x+y;
     }
